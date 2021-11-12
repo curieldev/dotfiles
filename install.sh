24e29fbfeec37256d5ad60af2fd65654a50f5f4d
curieldev <curieldev@gmail.com>
2021-11-12 11:15:37 -0600

    Added sxhkdrc file

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 ln -sv --backup=numbered $USER_HOME/git/dotfiles/xorg/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
+sudo -u $SUDO_USER ln -sv --backup=numbered $USER_HOME/git/dotfiles/sxhkd/sxhkdrc $USER_HOME/.config/sxhkd/sxhkdrc
