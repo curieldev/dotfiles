9745e2c9ef9f5cd4dffd912106e23e97884b96d6
curieldev <curieldev@gmail.com>
2021-11-13 08:23:30 -0600

    Added neovim init file and cleaned up script
    Some Paths and URLs were converted to variables to clean up a little
    bit.
    Basic init.vim configuration added.

--- install.sh
+++ install.sh
@@ -3,18 +3,33 @@
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 # Enable TearFree Option
-ln -sv --backup=numbered $USER_HOME/git/dotfiles/xorg/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
+XORG_CONF_PATH=/etc/X11/xorg.conf.d/
 
-# Configure sxhkdrc key bindkings
-sudo -u $SUDO_USER ln -sv --backup=numbered $USER_HOME/git/dotfiles/sxhkd/sxhkdrc $USER_HOME/.config/sxhkd/sxhkdrc
+ln -sv --backup=numbered $USER_HOME/git/dotfiles/xorg/20-intel.conf $XORG_CONF_PATH/20-intel.conf
+
+# Configure sxhkdrc key bindings
+SXHKD_CONFIG_PATH=$USER_HOME/.config/sxhkd
+sudo -u $SUDO_USER ln -sv --backup=numbered $USER_HOME/git/dotfiles/sxhkd/sxhkdrc $SXHKD_CONFIG_PATH/sxhkdrc
 
 # Set LightDM Aether background image
-rm -f /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers/{*.png,*.jpeg,*.jpg}
-curl https://raw.githubusercontent.com/curieldev/resources/main/img/Blue-PaperMario.jpg -o /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers/Blue-PaperMario.jpg
+AETHER_WALLPAPERS_PATH=/usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers
+AETHER_WALLPAPER_URL=https://raw.githubusercontent.com/curieldev/resources/main/img/Blue-PaperMario.jpg
+
+rm -vf $AETHER_WALLPAPERS_PATH/{*.png,*.jpeg,*.jpg}
+curl $AETHER_WALLPAPER_URL -o $AETHER_WALLPAPERS_PATH/Blue-PaperMario.jpg
 
 # Set LightDM Aether icon
-sed -i "s/^Icon=.*/Icon=\/var\/lib\/AccountsService\/icons\/$SUDO_USER.png/" /var/lib/AccountsService/users/$SUDO_USER
-curl https://raw.githubusercontent.com/curieldev/resources/main/img/curiel.png -o /var/lib/AccountsService/icons/$SUDO_USER.png
+ACCOUNTS_SVC_PATH=/var/lib/AccountsService
+AETHER_ICON_URL=https://raw.githubusercontent.com/curieldev/resources/main/img/curiel.png
+
+sed -i "s/^Icon=.*/Icon=\/var\/lib\/AccountsService\/icons\/$SUDO_USER.png/" $ACCOUNTS_SVC_PATH/users/$SUDO_USER
+curl $AETHER_ICON_URL -o $ACCOUNTS_SVC_PATH/icons/$SUDO_USER.png
 
 # Set EndeavourOS wallpaper
-curl https://raw.githubusercontent.com/curieldev/resources/main/img/endeavouros-wallpaper.png -o /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+ENDEAVOUROS_WALLPAPER_URL=https://raw.githubusercontent.com/curieldev/resources/main/img/endeavouros-wallpaper.png
+ENDEAVOUROS_BACKGROUNDS_PATH=/usr/share/endeavouros/backgrounds
+curl $ENDEAVOUROS_WALLPAPER_URL -o $ENDEAVOUROS_BACKGROUNDS_PATH/endeavouros-wallpaper.png
+
+
+# Configure neovim
+sudo -u $SUDO_USER cp -v --backup=numbered --parents $USER_HOME/git/dotfiles/nvim/init.vim $USER_HOME/.config/
